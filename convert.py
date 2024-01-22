92b0134250c03dec1b4bc1bfa9991081b179f38c
wwiras <wwiras@gmail.com>
2024-01-22 12:47:50 +0800

    update code desc

--- convert.py
+++ convert.py
@@ -2,24 +2,18 @@ import sys
 import bibtexparser
 import pandas as pd
 
+#check 3 arguments at least
 if not len(sys.argv)==3:
     print("Need more arguments")
-elif not sys.argv[1].endswith(".bib"):
+elif not sys.argv[1].endswith(".bib"): #check bib file
     print("No bib file specified")
-elif not sys.argv[2].endswith(".csv"):
+elif not sys.argv[2].endswith(".csv"): #check csv file
     print("No csv file specified")
 else:
-    # for n,arg in enumerate(sys.argv):
-    #     print(n,arg)
+    # Migrating starts here....
     bibtex_filename=sys.argv[1]
     csv_filename=sys.argv[2]
     with open(bibtex_filename) as bibtex_file:
         bib_database = bibtexparser.load(bibtex_file)
     df = pd.DataFrame(bib_database.entries)
     df.to_csv(csv_filename, index=False)
-
-
-# with open('ref.bib') as bibtex_file:
-#   bib_database = bibtexparser.load(bibtex_file)
-# df = pd.DataFrame(bib_database.entries)
-# df.to_csv('ref.csv', index=False)
